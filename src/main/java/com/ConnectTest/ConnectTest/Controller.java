f292f66260264c5bdd9e14b133b317b6b5858509
JungGyu Ahn(안중규) <newending01@gmail.com>
2022-08-23 20:03:42 +0900

    feat: Add JSON response data

--- src/main/java/com/ConnectTest/ConnectTest/Controller.java
+++ src/main/java/com/ConnectTest/ConnectTest/Controller.java
@@ -1,15 +1,35 @@
 package com.ConnectTest.ConnectTest;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ConnectTest.ConnectTest.data.JsonData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
+@RequestMapping(value = "/", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class Controller {
 
+    private final Map<String, String> testContents = new HashMap<>();
+
     @GetMapping("/")
-    public String Test() {
-        return "Hello";
+    public ResponseEntity Test() {
+        System.out.println("[응답 테스트]");
+
+        return new ResponseEntity<>(testContents, HttpStatus.OK);
+    }
+
+    @PostConstruct
+    public void init() {
+        JsonData jsonData = new JsonData();
+        jsonData.setKey("test");
+        jsonData.setValue("success");
+
+        testContents.put(jsonData.getKey(), jsonData.getValue());
     }
 }
